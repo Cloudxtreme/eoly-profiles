8da45682c9dff507eb55266000de454304058989
Eric Olsen <eric@ericolsen.net>
2014-11-06 21:33:12 -0500

    Setup local users.

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,11 +1,15 @@
 class profiles::base {
 
   $ntp_servers = hiera('ntp_servers')
+  $local_users = hiera('local_users')
 
   validate_array($ntp_servers)
+  validate_hash($local_users)
 
   class { 'ntp':
     servers => $ntp_servers,
   }
+
+  create_resources(user, $local_users)
 
 }
