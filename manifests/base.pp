9a531611d5a4c55a8301f6df3fba8b7371463d17
Eric Olsen <eric@ericolsen.net>
2014-11-10 22:32:04 -0500

    Manage dot vim dir from git repo.

--- manifests/base.pp
+++ manifests/base.pp
@@ -2,6 +2,7 @@ class profiles::base {
 
   $ntp_servers = hiera('ntp_servers')
   $local_users = hiera('local_users')
+  $vimrc_repo  = hiera('vimrc_repo')
 
   validate_array($ntp_servers)
   validate_hash($local_users)
@@ -11,5 +12,10 @@ class profiles::base {
   }
 
   create_resources(user, $local_users)
+
+  $vimrc_defaults = {
+    'require' => User['eolsen']
+  }
+  create_resources(vcsrepo, $vimrc_repo, $vimrc_defaults)
 
 }
