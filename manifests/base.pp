711bfced71b5ae794a2201eaf977465050f1e56a
Eric Olsen <eric@ericolsen.net>
2014-11-23 10:58:12 -0500

    Moved eolsen user to dedicated profile.

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,21 +1,11 @@
 class profiles::base {
 
   $ntp_servers = hiera('ntp_servers')
-  $local_users = hiera('local_users')
-  $vimrc_repo  = hiera('vimrc_repo')
 
   validate_array($ntp_servers)
-  validate_hash($local_users)
 
   class { 'ntp':
     servers => $ntp_servers,
   }
-
-  create_resources(user, $local_users)
-
-  $vimrc_defaults = {
-    'require' => User['eolsen']
-  }
-  create_resources(vcsrepo, $vimrc_repo, $vimrc_defaults)
 
 }
---
+++ manifests/eolsen.pp
@@ -0,0 +1,24 @@
+class profiles::eolsen {
+
+  $eolsen_user = hiera('eolsen_user')
+
+  validate_hash($eolsen_user)
+  validate_string($eolsen_user[password])
+  validate_array($eolsen_user[groups])
+
+  user { 'eolsen':
+    ensure    => present,
+    password  => $eolsen_user[password],
+    groups    => $eolsen_user[groups],
+    mangehome => true,
+    home      => '/home/eolsen',
+  }
+
+  vcsrepo { '/home/eolsen/.vim':
+    ensure   => present,
+    provider => 'git',
+    source   => 'https://github.com/eoly/eoly-vimrc.git',
+    require  => User['eolsen'],
+  }
+
+}
