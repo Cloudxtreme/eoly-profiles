7b66d271f18416cc4c5e47f1d9416214a3c82e12
Eric Olsen <eric@ericolsen.net>
2015-01-18 11:16:37 -0500

    updated redis profile

--- manifests/redis.pp
+++ manifests/redis.pp
@@ -6,11 +6,12 @@ class profiles::redis {
   contain '::redis::install'
 
   redis::server {
-    redis_name   => 'logstash',
-    redis_memory => '1g',
-    redis_ip     => '0.0.0.0',
-    redis_port   => 6379,
-    running      => true,
+    'logstash':
+      redis_name   => 'logstash',
+      redis_memory => '1g',
+      redis_ip     => '0.0.0.0',
+      redis_port   => 6379,
+      running      => true,
   }
   
 }
