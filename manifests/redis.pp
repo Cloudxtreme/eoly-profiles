7e540e33981a95cdc16f5d36c344e9d5f64f247b
Eric Olsen <eric@ericolsen.net>
2015-01-18 11:14:05 -0500

    added redis profile

---
+++ manifests/redis.pp
@@ -0,0 +1,16 @@
+class profiles::redis {
+
+  class { '::redis::install':
+    redis_package => true,
+  }
+  contain '::redis::install'
+
+  redis::server {
+    redis_name   => 'logstash',
+    redis_memory => '1g',
+    redis_ip     => '0.0.0.0',
+    redis_port   => 6379,
+    running      => true,
+  }
+  
+}
