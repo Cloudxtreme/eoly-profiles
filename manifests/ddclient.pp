4ef58a045de3c8c4270810f5f0738f7d74aa1967
Eric Olsen <eric@ericolsen.net>
2015-01-18 21:08:55 -0500

    updated ddlcient profile

--- manifests/ddclient.pp
+++ manifests/ddclient.pp
@@ -2,8 +2,12 @@ class profiles::ddclient {
 
  $source_url = "https://github.com/wimpunk/ddclient/archive/master.zip"
 
- staging::file { 'ddclient':
+ staging::file { 'ddclient.zip':
   source => $::profiles::ddclient::source_url
+ }
+
+ staging::extract { 'ddclient.zip':
+  require => Staging::File['ddclient.zip'],
  }
   
 }
