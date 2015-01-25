4f1a6385c9660c26d47f9eef443f0d388da94554
Eric Olsen <eric@ericolsen.net>
2015-01-25 12:25:13 -0500

    manage logstash redis output

--- manifests/logstash.pp
+++ manifests/logstash.pp
@@ -1,4 +1,7 @@
 class profiles::logstash {
+
+  $logstash_es_server     = "redis.hq.sourcecurrent.net"
+  $logstash_es_cluster    = "logstash"
  
   include '::profiles::java'
 
@@ -10,6 +13,11 @@ class profiles::logstash {
     java_install      => false,
     autoupgrade       => false,
     restart_on_change => false,
+  }
+
+  logstash::configfile { 'output_redis':
+    content => template("${module_name}/logstash/output_redis.conf.erb"),
+    order => 30
   }
 
 }
